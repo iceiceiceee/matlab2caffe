3d0bc5500e86ac1a653d8117c8c3f56788dc5434
iceiceiceee <menglg22@outlook.com>
2018-08-12 17:09:47 +0800

    Add files via upload

---
+++ genCFmodel.cpp
@@ -0,0 +1,36 @@
+#include <caffe/caffe.hpp>
+#include <google/protobuf/io/coded_stream.h>
+#include <google/protobuf/io/zero_copy_stream_impl.h>
+#include <google/protobuf/text_format.h>
+#include <algorithm>
+#include <iosfwd>
+#include <memory>
+#include <string>
+#include <utility>
+#include <vector>
+#include <iostream>
+#include "caffe/common.hpp"
+#include "caffe/proto/caffe.pb.h"
+#include "caffe/util/io.hpp"
+#include <stdlib.h> 
+
+using namespace caffe;
+using namespace std;
+using google::protobuf::io::FileInputStream;
+using google::protobuf::io::FileOutputStream;
+using google::protobuf::io::ZeroCopyInputStream;
+using google::protobuf::io::CodedInputStream;
+using google::protobuf::io::ZeroCopyOutputStream;
+using google::protobuf::io::CodedOutputStream;
+using google::protobuf::Message;
+
+int main()
+{
+	NetParameter proto;
+	ReadProtoFromTextFile("G:/py/tf2caffe/genmodel/alex2.prototxt", &proto);
+	WriteProtoToBinaryFile(proto, "G:/py/tf2caffe/genmodel/alexnet.caffemodel");
+	printf("success");
+	system("PAUSE");
+
+	return 0;
+}
